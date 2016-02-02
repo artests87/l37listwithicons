68b3a8e7e47a08796744d94ed28381e7ff62ed41
artests <illusors1>
2016-02-02 17:08:15 +0300

    C2

--- app/src/main/java/com/example/artests/l37listwithicons/ListViewWithIconsActivity.java
+++ app/src/main/java/com/example/artests/l37listwithicons/ListViewWithIconsActivity.java
@@ -1,10 +1,15 @@
 package com.example.artests.l37listwithicons;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -13,16 +18,20 @@ import android.widget.Toast;
 public class ListViewWithIconsActivity extends ListActivity {
 
     // определяем массив типа String
-    private final String[] mCatNames = new String[] { "Рыжик", "Барсик", "Мурзик",
+    /*private final String[] mCatNames = new String[] { "Рыжик", "Барсик", "Мурзик",
             "Мурка", "Васька", "Томасина", "Кристина", "Пушок", "Дымка",
-            "Кузя", "Китти", "Масяня", "Симба" };
-
+            "Кузя", "Китти", "Масяня", "Симба" };*/
+    // Строка, которую мы выводим в список
+    private String[] mSignArray = { "Овен", "Телец", "Близнецы", "Рак", "Лев", "Дева",
+            "Весы", "Скорпион", "Стрелец", "Козерог", "Водолей", "Рыбы" };
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setListAdapter(new ArrayAdapter<>(this, R.layout.list_item,
-                R.id.textViewCatName, mCatNames));
+        /*setListAdapter(new ArrayAdapter<>(this, R.layout.list_item,
+                R.id.textViewCatName, mCatNames));*/
+        // меняем на свой адаптер
+        setListAdapter(new MyCustomAdapter(this, R.layout.list_item, mSignArray));
     }
 
     @Override
@@ -30,5 +39,30 @@ public class ListViewWithIconsActivity extends ListActivity {
         // super.onListItemClick(l, v, position, id);
         String selection = l.getItemAtPosition(position).toString();
         Toast.makeText(this, selection, Toast.LENGTH_LONG).show();
+    }
+    // Пишем свой класс-адаптер
+    public class MyCustomAdapter extends ArrayAdapter<String> {
+
+        public MyCustomAdapter(Context context, int textViewResourceId,
+                               String[] objects) {
+            super(context, textViewResourceId, objects);
+            // TODO Auto-generated constructor stub
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View row = inflater.inflate(R.layout.list_item, parent, false);
+            TextView label = (TextView) row.findViewById(R.id.textViewSign);
+            label.setText(mSignArray[position]);
+            ImageView iconImageView = (ImageView) row.findViewById(R.id.imageViewIcon);
+            // Если текст содержит кота, то выводим значок Льва
+            if (mSignArray[position] == "Лев") {
+                iconImageView.setImageResource(R.drawable.lion);
+            } else {
+                iconImageView.setImageResource(R.drawable.ic_launcher);
+            }
+            return row;
+        }
     }
 }
